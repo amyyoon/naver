3ab533c9589bd66420723bfd33d14dab4995943c
JEONGEUN YOON <soleeyaa@gmail.com>
2018-11-03 23:04:11 +0900

    keywords slider

--- js/src/naver_main.js
+++ js/src/naver_main.js
@@ -5,8 +5,8 @@
 
 	setInterval(function(){
 		if(j<3){j+=1}else{j=0}
-		topLi.animate({marginTop:j*-25+'%'})
-		topLi.css({display:'none'})
+		topLi.animate({marginTop:j*-30+'px'})
+		topLi.eq(0).next().css({display:'block'})
 	},3000);
 console.log(topLi);
 
