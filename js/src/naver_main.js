c31b9a3cb36bbaed6755280bdddc2280eb4f5803
JEONGEUN YOON <soleeyaa@gmail.com>
2018-11-04 14:28:16 +0900

    keywords

--- js/src/naver_main.js
+++ js/src/naver_main.js
@@ -1,13 +1,14 @@
 (function(){
-
+// 키워드 슬라이드
 	var topLi = $('.top').children('li');
 	var j=0;
 
 	setInterval(function(){
 		if(j<3){j+=1}else{j=0}
 		$('.top').animate({top:j*-100+'%'})
-		$('.top').children('li').next().css({display:'none'})
+		$('.top').children('li').css({display:'block'})
 	},3000);
-console.log(topLi);
+
+
 
 })(jQuery);
